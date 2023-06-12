1a61ef537463c227940346a7b59f0d6d2d1f5d17
RamonJacques <ramonjacquesrs@gmail.com>
2023-06-12 13:54:26 -0300

    Possible moves Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargerPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}	
@@ -40,13 +41,20 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
-	public void validateSourcePosition (Position position){
+	public void validateSourcePosition (Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on sorce position.");
 		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
+	}
+	
+	private void validateTargerPosition (Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position.");
+		}
+		
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
